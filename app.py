76d4f8eb2fe5ac1169c2582fea9159e220a41a93
Steven <steven2947@163.com>
2018-01-26 15:02:39 +0800

    wechat 验证token

--- app.py
+++ app.py
@@ -1,21 +1,35 @@
 # coding: utf-8
 import hashlib
+import leancloud
 from datetime import datetime
 
-from flask import Flask, make_response
+from flask import Flask
 from flask import render_template
 from flask import request
-import leancloud
 from flask_sockets import Sockets
-
 from views.todos import todos_view
+import xml.etree.ElementTree as ET
 
 app = Flask(__name__)
 sockets = Sockets(app)
 
+wechat_token = 'steven2947'
+
 # 动态路由
 app.register_blueprint(todos_view, url_prefix='/todos')
 
+
+# 检查签名正确性的
+def check_signature(signature, timestamp, nonce):
+    token = wechat_token
+    tmp_arr = [token, timestamp, nonce]
+    tmp_arr.sort()
+    tmp_str = tmp_arr[0] + tmp_arr[1] + tmp_arr[2]
+    sha1_tmp_str = hashlib.sha1(tmp_str).hexdigest()
+    if (sha1_tmp_str == signature) :
+        return True
+    else :
+        return False
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -44,3 +58,27 @@ def get_json():
 def save():
     user_name = request.args.get('user_name')
     return r'{"hello": "' + user_name + r'"}'
+
+
+@app.route('/wechat/', methods=['GET', 'POST'])
+def wechat():
+    signature = request.args.get('signature', '')
+    timestamp = request.args.get('timestamp', '')
+    nonce = request.args.get('nonce', '')
+    echostr = request.args.get('echostr', '')
+
+    if request.method == 'GET':
+        if check_signature(signature, timestamp, nonce) :
+            return echostr
+        else :
+            return 'Not Valid!'
+    else :
+        # if check_signature(signature, timestamp, nonce) :
+        xml_recv = ET.fromstring(request.data)
+        ToUserName = xml_recv.find("ToUserName").text
+        FromUserName = xml_recv.find("FromUserName").text
+        Content = xml_recv.find("Content").text
+        reply = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content><FuncFlag>0</FuncFlag></xml>"
+        re_msg = (reply % (FromUserName, ToUserName, str(int(time.time())), Content))
+        return re_msg
+
